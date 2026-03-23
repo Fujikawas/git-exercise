91aabf6d196ee7a427d29a57433c3258791f119b
Amirdorian1 <amir.bani-mohammad-ali@mib.uni-stuttgart.de>
2026-03-23 17:09:46 +0100

    Ensure  a vector use a copy (#2)
    Co-authored-by: Amir Bani Mohammad Ali <amirb@MIB40-TESLA.uni-stuttgart.de>

--- exercise1.py
+++ exercise1.py
@@ -7,8 +7,8 @@ from math import isclose
 
 class Vector:
     def __init__(self, coordinates: List[float]) -> None:
-        # self._coordinates = coordinates.copy()
-        self._coordinates = coordinates # Task A
+        # Use a copy of the input list to avoid modifying the original data
+        self._coordinates = coordinates.copy()
 
     def __getitem__(self, i: int) -> float:
         return self._coordinates[i]
